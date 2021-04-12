5939c30dc00a412733230f89171d1e5ce750fe08
herui <rui.he@haiziwang.com>
2021-04-12 14:33:57 +0800

    测试用例中不固定配置

--- common/orm/orm_test.go
+++ common/orm/orm_test.go
@@ -11,12 +11,7 @@ import (
 func TestInitOrm(t *testing.T) {
 	_, err := orm.GetDb()
 	require.NotNil(t, err)
-	err = orm.InitOrm(
-		orm.DriverName("mysql"),
-		orm.Dsn("herui:Admin@123@tcp(172.172.177.191:3306)/onesite_dev?charset=utf8mb4&parseTime=true"),
-		orm.MaxOpenConn(6),
-		orm.MaxIdleConn(3),
-	)
+	err = orm.InitOrm()
 	require.Nil(t, err)
 	_, err = orm.GetDb()
 	require.Nil(t, err)
--- common/redis/redis_test.go
+++ common/redis/redis_test.go
@@ -11,11 +11,7 @@ import (
 func TestInitRedis(t *testing.T) {
 	_, err := redis.GetRedis()
 	require.NotNil(t, err)
-	err = redis.InitRedis(
-		redis.Addr("172.172.177.191:6379"),
-		redis.Password(""),
-		redis.Db(0),
-	)
+	err = redis.InitRedis()
 	require.Nil(t, err)
 	cli, _ := redis.GetRedis()
 	require.NotNil(t, cli)
